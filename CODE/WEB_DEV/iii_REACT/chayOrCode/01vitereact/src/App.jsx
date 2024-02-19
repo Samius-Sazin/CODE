92f52a03296544414a95f9c3675fe27f2b6d7710
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2024-02-19 09:21:44 +0600

    Add files via upload

---
+++ CODE/WEB_DEV/iii_REACT/chayOrCode/01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+import LearningReact from "./sami";
+
+function App() {
+
+  const tryVariable = 'React is fun !!';
+
+  return (
+    <>
+      <h1>This is App.jsx component & {tryVariable}</h1>
+
+      {/* <LearningReact></LearningReact> */}
+      <LearningReact />
+
+    </>
+  )
+}
+
+export default App
---
+++ CODE/WEB_DEV/iii_REACT/chayOrCode/01vitereact/src/sami.jsx
@@ -0,0 +1,9 @@
+function LearningReact(){
+    return (
+        <>
+            <h1>This is LearningReact component from sami.jsx</h1>
+        </>
+    )
+}
+
+export default LearningReact
