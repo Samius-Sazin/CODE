e3176a889e7ee5c853102c67728a1d347356d88a
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2025-04-29 22:01:42 +0600

    Add files via upload

---
+++ Programming/STL_C++/6_STACK_QUEUE/4_Find_The_Next_Greater_Element.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int *arr, int n);
+
+int main()
+{
+    int arr[] = {4, 5, 2, 25, 7, 8};
+    int n = sizeof(arr)/sizeof(int);
+
+    solve(arr, n);
+}
+
+void solve(int *arr, int n)
+{
+    stack <int> s;
+    vector <int> v;
+
+    for(int i=0; i<n; i++)
+    {
+        
+    }
+
+}
